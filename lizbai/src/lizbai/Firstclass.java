4a55f700b7c32ce987b3db120bedb85cc61d1313
lizbai <callmetinywhite@gmail.com>
2014-10-16 13:17:44 +0800

    lizbai added a cout command in lizbai firstclass 1317

--- lizbai/src/lizbai/Firstclass.java
+++ lizbai/src/lizbai/Firstclass.java
@@ -9,7 +9,11 @@ public class Firstclass {
 		//edit again by ranranb
 
 		//lizbai leaves a comment 1142
+<<<<<<< Upstream, based on branch 'master' of https://github.com/lizbai/hello-world
 
+=======
+		System.out.println("hello world");
+>>>>>>> 563485b lizbai added a cout command in lizbai firstclass 1317
 	}
 
 }
